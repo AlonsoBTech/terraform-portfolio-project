e75cf46457a4b2334f883f5f7b7e79755dd9e247
AlonsoBTech <lonsobraithwaite1996@gmail.com>
2024-07-08 20:51:00 -0400

    Third Commit: Updated Terraform main.tf file and added cloudfront distribution

--- aeb-blog/terraform-js/main.tf
+++ aeb-blog/terraform-js/main.tf
@@ -79,3 +79,53 @@ resource "aws_s3_bucket_policy" "nextjs_bucket_policy" {
     ]
   }))
 }
+
+# CloudFront Origin Access Identity
+resource "aws_cloudfront_origin_access_identity" "cdn_origin_access_identity" {
+  comment = "Origin Access Identity for Next.js portfolio website"
+}
+
+# CloudFront Distribution
+resource "aws_cloudfront_distribution" "nextjs_cdn_distribution" {
+    origin {
+        domain_name = aws_s3_bucket.nextjs_bucket.bucket_regional_domain_name
+        origin_id = "s3-nextjs-portfolio--bucket"
+
+        s3_origin_config {
+            origin_access_identity = aws_cloudfront_origin_access_identity.cdn_origin_access_identity
+        }
+    }
+
+    enabled = true 
+    is_ipv6_enabled = true 
+    comment = "AEB-Blog Next.js portfolio site"
+    default_root-object = "index.html"
+
+    default_cache_behavior {
+        allowed_methods = [ "GET", "HEAD", "OPTIONS"]
+        cached_methods = ["GET", "HEAD"]
+        target_origin_id = "s3-nextjs-portfolio-bucket"
+
+        forwarded_values {
+            query_string = false
+            cookies {
+                forward = "none"
+            }
+        }
+
+        viewer_protocol_policy = "redirect-to-https"
+        min_ttl = 0
+        default_ttl = 3600
+        max_ttl = 86400
+    }
+
+    restrictions {
+        geo_restrictions {
+            restriction_type = none
+        }
+    }
+
+    viewer_certificate {
+        cloudfront_default_certificate = true 
+    }
+}
